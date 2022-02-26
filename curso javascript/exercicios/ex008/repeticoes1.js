277c5cbb0fd36de881ae233316ef97042d52aa01
Lucas Renan <lucasrenandns96@gmail.com>
2022-02-26 18:24:50 -0300

    update

---
+++ curso javascript/exercicios/ex008/repeticoes1.js
@@ -0,0 +1,15 @@
+var c = 1
+do {
+    console.log(c)
+    c++
+}while (c <= 20)
+
+
+
+/*
+var c = 1
+while (c <= 10) {
+    console.log(c)
+    c++
+}console.log('Fim do programa!')
+*/
---
+++ curso javascript/exercicios/ex008/repeticoes3.js
@@ -0,0 +1,14 @@
+/*
+var c = 1
+do {
+    console.log(c)
+    c++
+}while (c <= 5)
+*/
+
+console.log('Vai comecar... ')
+for (var c = 1; c <= 4; c++) {
+    console.log(c)
+    
+}
+console.log('Fim')
