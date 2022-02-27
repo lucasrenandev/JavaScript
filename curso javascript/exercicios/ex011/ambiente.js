dcb16fddeb118a023f444ae8bc14f65e1f4420d7
Lucas Renan <lucasrenandns96@gmail.com>
2022-02-27 15:14:28 -0300

    vetores

---
+++ curso javascript/exercicios/ex011/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 2, 9, 3]
+
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} elementos`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(num[3])
