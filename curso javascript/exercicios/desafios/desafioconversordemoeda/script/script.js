217164ce375bc96d10d71a0361d4f0195cf85eaf
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-09 15:41:48 -0300

    conversor de moeda

---
+++ curso javascript/exercicios/desafios/desafioconversordemoeda/script/script.js
@@ -0,0 +1,12 @@
+window.prompt('Antes de mais nada, qual a cotação do Dolar atual?')
+
+function conversormoeda() {
+    var real = parseFloat(window.prompt('Quantos R$ você tem na carteira? '))
+    var res = document.getElementById('res')
+
+    dolar = (real / 5).toFixed(2) 
+    euro = (real / 5.54).toFixed(2)
+
+    res.innerHTML = `Com R$${real} você pode comprar US$${dolar} (Dolar) <br> E tambêm pode comprar £ ${euro} (Euro)`
+
+}
