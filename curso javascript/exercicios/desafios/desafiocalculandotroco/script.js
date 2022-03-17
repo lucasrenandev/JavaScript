b7c4354ba9ce36ea40fdf908f0afc1677c4540d8
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-17 20:52:35 -0300

    Update script.js

--- curso javascript/exercicios/desafios/desafiocalculandotroco/script.js
+++ curso javascript/exercicios/desafios/desafiocalculandotroco/script.js
@@ -1,11 +1,11 @@
 function troco() {
     let compra = window.prompt('Qual produto você esta comprando? ')
-    let preco = parseFloat(window.prompt(`Quanto custa o produto ${compra} que você esta comprando? `))
-    let valor = parseFloat(window.prompt(`Qual foi o valor que você deu para pagar o produto ${compra}? R$`))
+    let preco = Number(window.prompt(`Quanto custa o produto ${compra} que você esta comprando? `))
+    let valor = Number(window.prompt(`Qual foi o valor que você deu para pagar o produto ${compra}? R$`))
 
     preco = preco
-    retorno = (valor - preco).toFixed(2)
+    retorno = valor - preco
     
-    window.alert(`Você comprou o produto ${compra} que custou R$${preco.toFixed(2)}.\nVocê deu R$${valor} em dinheiro e vai receber R$${retorno} de troco.\nVolte sempre!`)
+    window.alert(`Você comprou o produto ${compra} que custou R$${preco.toFixed(2)}.\nVocê deu R$${valor} em dinheiro e vai receber R$${retorno.toFixed(2)} de troco.\nVolte sempre!`)
     
 }
