b206a2f94c0f14f03f53d404d27005f25db2cbc3
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-17 19:17:14 -0300

    Update script.js

--- curso javascript/exercicios/desafios/desafiocalculandodesconto/script.js
+++ curso javascript/exercicios/desafios/desafiocalculandodesconto/script.js
@@ -1,11 +1,16 @@
 function desconto() {
     let produto = (window.prompt('Qual o produto que você esta comprando?'))
-    let preço = parseFloat(window.prompt(`Qual o preço do produto ${produto}?`))
+    let preço = Number(window.prompt(`Qual o preço do produto ${produto}?`))
     let res = document.getElementById('res')
 
-    originalpreco = preço.toFixed(2)
+    originalpreco = preço
     desc = (preço * 10 / 100)
     novopreco = preço - (preço * 10 / 100)
 
-    res.innerHTML = `Calculando desconto de 10% para ${produto}... <br> O preço original era R$${originalpreco}. <br> Você acaba de ganhar ${desc.toFixed(2)} de desconto (-10%). <br> No fim, você vai pagar R$${novopreco.toFixed(2)} no produto ${produto}`
+    res.innerHTML = `<h2>Calculando desconto de 10% para ${produto} ...</h2>`
+    res.innerHTML += `<p>O preço original era R$ ${originalpreco.toFixed(2)}.</p>`
+    res.innerHTML += `<p>Você acaba de ganhar R$ ${desc.toFixed(2)} de desconto.</p>`
+    res.innerHTML += `<p>No fim, você vai pagar R$ ${novopreco.toFixed(2)} no produto ${produto}.</p>`
+    
+
 }
