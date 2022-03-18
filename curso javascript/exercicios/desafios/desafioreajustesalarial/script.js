eb17429b7529e3f4a1de6d4d7fe85cee8b022779
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-18 10:13:31 -0300

    Update script.js

--- curso javascript/exercicios/desafios/desafioreajustesalarial/script.js
+++ curso javascript/exercicios/desafios/desafioreajustesalarial/script.js
@@ -3,11 +3,14 @@ function ReajusteSalarial() {
     var salário = Number(window.prompt('Qual o salário de ' + nome + '?'))
     var reajuste = Number(window.prompt('O salário de ' + nome + ' vai ser reajustado em qual porcentagem?'))
     var res = document.getElementById('res')
-    var resn = document.getElementById('resn')
 
     var aumento = (salário * reajuste) / 100
-    novosalario = salário + aumento
+    novosalário = salário + aumento
 
-    resn.innerHTML = 'O funcionário ' + nome + ' recebeu um aumento!'
-    res.innerHTML = `O salário atual do funcionário é ${salário.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} <br> Com um aumento de ${reajuste}%, o salário irá aumentar ${aumento.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} no próximo mês. <br> E a partir dai, ${nome} passará a ganhar R$${novosalario.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}!`
+
+    res.innerHTML = `<h2>O funcionário ${nome.toUpperCase()} recebeu um aumento!</h2>`
+    res.innerHTML += `<p>O salário atual do funcionário é ${salário.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}.</p>`
+    res.innerHTML += `<p>Com um aumento de ${reajuste}%, o salário irá aumentar ${aumento.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} no próximo mês.</p>`
+    res.innerHTML += `<p>E a partir dai, ${nome.toUpperCase()} passará a receber ${novosalário.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}!</p>` 
+
 }
