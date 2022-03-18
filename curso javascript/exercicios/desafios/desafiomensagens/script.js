e90b29f52fcedd15fc66336d0536a1c78b776c07
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-17 21:39:17 -0300

    Update script.js

--- curso javascript/exercicios/desafios/desafiomensagens/script.js
+++ curso javascript/exercicios/desafios/desafiomensagens/script.js
@@ -1,9 +1,13 @@
 function b1() {
+    
     window.alert('Você clicou no 1º Botão!')
 }
 function b2() {
+
     window.alert('Você clicou no 2º Botão!')
 }
 function b3() {
+
     window.alert('Você clicou no 3º Botão!')
+
 }
