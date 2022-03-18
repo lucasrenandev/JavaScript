753778ed0cad11748349f5f8482e48673f4f498a
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-18 09:49:00 -0300

    Update script.js

--- curso javascript/exercicios/desafios/desafiosituacaodoaluno/script.js
+++ curso javascript/exercicios/desafios/desafiosituacaodoaluno/script.js
@@ -5,19 +5,19 @@ function verificarNotas() {
     const res = document.getElementById('res')
     
 
-    média = (nota1 + nota2).toFixed(2) / 2
+    média = (nota1 + nota2) / 2
 
     let msg 
     if (média > 6) {
-        msg = `Com média acima de <mark>6</mark>, o aluno está <strong style=color:green;>APROVADO!`
+        msg = `Com média acima de <mark>6</mark>, o aluno está <strong style=color:green;>APROVADO!</strong>`
     }else if (média >= 3 && (média <= 6)) {
         msg = `Com média entre <mark>3</mark> e <mark>6</mark>, o aluno está em <strong style=color:darkorange;>RECUPERAÇÃO!</strong>`
     }else if (média < 3) {
-        msg = `Com média abaixo de <mark>3</mark>, o aluno está <strong style=color:red;>REPROVADO!`
+        msg = `Com média abaixo de <mark>3</mark>, o aluno está <strong style=color:red;>REPROVADO!</strong>`
     }
     
-    res.innerHTML = `<p><strong>Analisando a situação de ${nome}...</strong></p>`.toUpperCase()
-    res.innerHTML += `<p>Com as notas <mark>${nota1}</mark> e <mark>${nota2}</mark> a média é <strong>${média}</strong></p>`
+    res.innerHTML = `<h2>Analisando a situação de ${nome.toUpperCase()}...</h2>` //método toUpperCase() = jogar para Maiúsculas.
+    res.innerHTML += `<p>Com as notas <mark>${nota1}</mark> e <mark>${nota2}</mark> a média é <strong>${média.toFixed(2)}</strong></p>` //método toFixed()= Selecionar quantas casas decimais deseja.
     res.innerHTML += msg
     
 }
