133fd62246a24df075fa0ebe4c9524a920eae14a
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-17 21:30:26 -0300

    Update script.js

--- curso javascript/exercicios/desafios/desafioconversordetemperatura/script.js
+++ curso javascript/exercicios/desafios/desafioconversordetemperatura/script.js
@@ -1,9 +1,11 @@
 function conversortemp() {
-    let cel = parseInt(window.prompt('Informe uma temperatura em °C (Celsius)'))
+    let cel = Number(window.prompt('Informe uma temperatura em °C (Celsius)'))
     let res = document.getElementById('res')
 
     let fahre = (cel * 1.8) + 32
     let kelvin = cel + 273.15
 
-    res.innerHTML = `A temperatura de ${cel}°C corresponde a... <br> ${fahre.toFixed(2)}°F (fahrenheit) <br> ${kelvin.toFixed(2)}°K (kelvin)`
+    res.innerHTML = `<h2>A temperatura de ${cel}°C corresponde a...</h2>`
+    res.innerHTML += `<p>${fahre}°F (Fahrenheit)</p>`
+    res.innerHTML += `<p>${kelvin}°K (kelvin)</p>`
 }
