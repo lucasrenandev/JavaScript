59e84d276aa72df1e75184a80a11e230af932a6b
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-17 21:21:04 -0300

    Update script.js

--- curso javascript/exercicios/desafios/desafioconversordemoeda/script.js
+++ curso javascript/exercicios/desafios/desafioconversordemoeda/script.js
@@ -1,12 +1,12 @@
 var cotação = Number(window.prompt('Antes de mais nada, qual a cotação do Dolar atual?'))
 
 function conversormoeda() {
-    var real = parseFloat(window.prompt('Quantos R$ você tem na carteira? '))
+    var real = Number(window.prompt('Quantos R$ você tem na carteira? '))
     let dolar = real / cotação
     var res = document.getElementById('res')
 
     res.innerHTML = '<h2>Conversão de Reais para Doláres</h2>'
-    res.innerHTML += `<p>Você tem ${real.toLocaleString('pt-BR')} na carteira.</p>`
+    res.innerHTML += `<p>Você tem ${real.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} na carteira.</p>`
     res.innerHTML += `<p>Considerando que $1.00 = ${cotação.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} na cotação informada.</p>`
     res.innerHTML += `<p>Com ${real.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} você pode comprar ${dolar.toLocaleString('en-US', {style: 'currency', currency: 'USD'})} Dólares.`
 
