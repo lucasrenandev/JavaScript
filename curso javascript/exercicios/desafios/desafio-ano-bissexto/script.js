b1b13843305246486f8314ea8c18fc4fcf1f8852
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-28 16:09:51 -0300

    Update script.js

--- curso javascript/exercicios/desafios/desafio-ano-bissexto/script.js
+++ curso javascript/exercicios/desafios/desafio-ano-bissexto/script.js
@@ -1,8 +1,8 @@
 function AnoBissexto() {
-    const ano = Number(window.prompt('Qual o ano que você quer verificar? '))
-    const res = document.querySelector('div#res')
+    let ano = Number(window.prompt('Qual o ano que você quer verificar? '))
+    let res = document.querySelector('div#res')
 
-    if(ano % 4 == 0 && (ano % 100 != 0 || (ano % 400 == 0) )) {
+    if(ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0 ) {
         res.innerHTML = `O ano de ${ano} é <strong> Bissexto </strong>`;
     }else {
         res.innerHTML = `O ano de ${ano} não é <strong> Bissexto </strong>`
